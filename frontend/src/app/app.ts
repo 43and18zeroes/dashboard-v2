9dfdcfe426b65ce10dd7bb338cd778f53ba53480
Christoph <c.wagner.germany@gmail.com>
2025-06-28 15:54:09 +0200

    css

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
