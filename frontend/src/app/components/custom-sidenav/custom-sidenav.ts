7217193d78d2d419e70925d92a15558a9f16f29b
Christoph <c.wagner.germany@gmail.com>
2025-06-29 16:56:24 +0200

    mat

--- frontend/src/app/components/custom-sidenav/custom-sidenav.ts
+++ frontend/src/app/components/custom-sidenav/custom-sidenav.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+export type menuItem = {
+  icon: string;
+  label: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-custom-sidenav',
-  imports: [],
+  imports: [MatListModule, MatIconModule],
   templateUrl: './custom-sidenav.html',
   styleUrl: './custom-sidenav.scss'
 })
 export class CustomSidenav {
 
+  menuItems = signal<menuItem[]>([
+
+  ])
 }
