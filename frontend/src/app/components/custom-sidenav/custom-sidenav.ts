33150bea2385e02ad45deb2a07696deee31256a0
Christoph <c.wagner.germany@gmail.com>
2025-06-29 17:12:08 +0200

    icons

--- frontend/src/app/components/custom-sidenav/custom-sidenav.ts
+++ frontend/src/app/components/custom-sidenav/custom-sidenav.ts
@@ -1,22 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
-export type menuItem = {
+export type MenuItem = {
   icon: string;
   label: string;
   route: string;
-}
+};
 
 @Component({
   selector: 'app-custom-sidenav',
-  imports: [MatListModule, MatIconModule],
+  imports: [CommonModule, MatListModule, MatIconModule, RouterModule],
   templateUrl: './custom-sidenav.html',
-  styleUrl: './custom-sidenav.scss'
+  styleUrl: './custom-sidenav.scss',
 })
+
 export class CustomSidenav {
-
-  menuItems = signal<menuItem[]>([
-
-  ])
+  menuItems: MenuItem[] = [
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: '/dashboard',
+    },
+    {
+      icon: 'settings',
+      label: 'Settings',
+      route: '/settings',
+    },
+    {
+      icon: 'info',
+      label: 'About',
+      route: '/about',
+    },
+  ];
 }
