906c8927e429d6018f697a5da231ce15da8ae0c4
Christoph <c.wagner.germany@gmail.com>
2025-06-30 16:52:45 +0200

    youtube folder

---
+++ yt/src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component, computed, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import { CustomSidenav } from "./components/custom-sidenav/custom-sidenav";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenav],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'frontend';
+
+  collapsed = signal(false);
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px')
+}
---
+++ yt/src/app/components/custom-sidenav/custom-sidenav.ts
@@ -0,0 +1,53 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, Input, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string;
+};
+
+
+@Component({
+  selector: 'app-custom-sidenav',
+  imports: [CommonModule, MatListModule, MatIconModule, RouterModule],
+  templateUrl: './custom-sidenav.html',
+  styleUrl: './custom-sidenav.scss',
+})
+
+
+export class CustomSidenav {
+
+  sideNavCollapsed = signal(false);
+  @Input() set collapsed(val: boolean) {
+    this.sideNavCollapsed.set(val)
+  }
+
+  profilePicSize = computed(() => this.sideNavCollapsed() ? '32' : '100'); 
+
+  menuItems: MenuItem[] = [
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: '/dashboard',
+    },
+    {
+      icon: 'ondemand_video',
+      label: 'Content',
+      route: '/content',
+    },
+    {
+      icon: 'bar_chart',
+      label: 'Analytics',
+      route: '/analytics',
+    },
+    {
+      icon: 'comment',
+      label: 'Comments',
+      route: '/comments',
+    },
+  ];
+}
