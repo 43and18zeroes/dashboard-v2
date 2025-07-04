e589086b7b0826066b77cb879e7d977f63328589
Christoph <c.wagner.germany@gmail.com>
2025-07-04 18:06:19 +0200

    css3

--- zoaib/src/app/pages/dashboard/dashboard.ts
+++ zoaib/src/app/pages/dashboard/dashboard.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [WidgetComponent, MatButtonModule, MatIconModule ],
   providers: [DashboardService],
   templateUrl: './dashboard.html',
