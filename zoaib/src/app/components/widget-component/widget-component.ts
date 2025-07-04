cb2cb9e3eb614b547f850c305f56acada11ecaef
Christoph <c.wagner.germany@gmail.com>
2025-07-04 16:20:46 +0200

    content

--- zoaib/src/app/components/widget-component/widget-component.ts
+++ zoaib/src/app/components/widget-component/widget-component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Widget } from '../../models/dashboard';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-widget-component',
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './widget-component.html',
   styleUrl: './widget-component.scss'
 })
