4556b174e1a7aa0b05339e4ee4bacb3ded63d2cf
Christoph <c.wagner.germany@gmail.com>
2025-07-04 16:39:20 +0200

    btn

--- zoaib/src/app/pages/dashboard/dashboard.ts
+++ zoaib/src/app/pages/dashboard/dashboard.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 import { WidgetComponent } from '../../components/widget-component/widget-component';
 import { Widget } from '../../models/dashboard';
 import { DashboardService } from '../../services/dashboard';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [WidgetComponent],
+  imports: [WidgetComponent, MatButtonModule, MatIcon],
   providers: [DashboardService],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
