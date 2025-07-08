43744737116c6fe086126b7e2a6bc71917015402
Christoph <c.wagner.germany@gmail.com>
2025-07-08 19:35:46 +0200

    showoptions

--- zoaib/src/app/components/widget-component/widget-component.ts
+++ zoaib/src/app/components/widget-component/widget-component.ts
@@ -1,13 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Widget } from '../../models/dashboard';
 import { NgComponentOutlet } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-widget-component',
-  imports: [NgComponentOutlet],
+  imports: [NgComponentOutlet, MatButtonModule, MatIconModule],
   templateUrl: './widget-component.html',
   styleUrl: './widget-component.scss',
 })
 export class WidgetComponent {
   data = input.required<Widget>();
+
+  showOptions = signal(false);
 }
