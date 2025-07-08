40e0785b842bf3dbecdf7ff771579fff02426ae7
Christoph <c.wagner.germany@gmail.com>
2025-07-08 19:58:05 +0200

    set

--- zoaib/src/app/components/widget-component/widget-options/widget-options.ts
+++ zoaib/src/app/components/widget-component/widget-options/widget-options.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-widget-options',
-  imports: [],
+  imports: [MatButtonModule, MatIcon],
   templateUrl: './widget-options.html',
   styleUrl: './widget-options.scss'
 })
 export class WidgetOptions {
-
+  showOptions = model<boolean>(false)
 }
