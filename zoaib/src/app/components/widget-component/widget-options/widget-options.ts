3d1d408f82e429e313bf56e56ef6502a5cadec69
Christoph <c.wagner.germany@gmail.com>
2025-07-08 20:20:11 +0200

    height

--- zoaib/src/app/components/widget-component/widget-options/widget-options.ts
+++ zoaib/src/app/components/widget-component/widget-options/widget-options.ts
@@ -1,10 +1,11 @@
 import { Component, model } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-widget-options',
-  imports: [MatButtonModule, MatIcon],
+  imports: [MatButtonModule, MatIcon, MatButtonToggleModule],
   templateUrl: './widget-options.html',
   styleUrl: './widget-options.scss'
 })
