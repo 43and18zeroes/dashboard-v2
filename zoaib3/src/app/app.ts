c1e8211b4af1a076c9b00545fd20e49cd0b5628d
Christoph <c.wagner.germany@gmail.com>
2025-07-10 05:51:49 +0200

    toolbar

--- zoaib3/src/app/app.ts
+++ zoaib3/src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
   template: `
+  <mat-toolbar class="mat-elevation-z3"></mat-toolbar>
     <router-outlet />
   `,
   styles: [],
