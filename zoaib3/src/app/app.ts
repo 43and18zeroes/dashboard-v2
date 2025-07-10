c5644ba25d0156331616227d279ea38d5c51c69b
Christoph <c.wagner.germany@gmail.com>
2025-07-10 05:59:21 +0200

    css

--- zoaib3/src/app/app.ts
+++ zoaib3/src/app/app.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
   template: `
-  <mat-toolbar class="mat-elevation-z3"></mat-toolbar>
-    <router-outlet />
+    <mat-toolbar class="mat-elevation-z3">
+      <button mat-icon-button>
+        <mat-icon>menu</mat-icon>
+      </button>
+    </mat-toolbar>
+    <mat-sidenav-container>
+      <mat-sidenav> </mat-sidenav>
+      <mat-sidenav-content class="content">
+        <router-outlet />
+      </mat-sidenav-content>
+    </mat-sidenav-container>
   `,
-  styles: [],
+  styles: [
+    `
+      mat-toolbar {
+        position: relative;
+        z-index: 5;
+      }
+
+      .content {
+        padding: 24px;
+      }
+
+      mat-sidenav-container {
+        height: calc(100vh - 64px);
+      }
+    `,
+  ],
 })
 export class App {
   protected title = 'zoaib3';
