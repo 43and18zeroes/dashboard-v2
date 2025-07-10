deeda618d1c7bde72e13d680d1df929e03e740df
Christoph <c.wagner.germany@gmail.com>
2025-07-10 06:34:52 +0200

    css2

--- zoaib3/src/app/components/custom-sidenav/custom-sidenav.ts
+++ zoaib3/src/app/components/custom-sidenav/custom-sidenav.ts
@@ -4,12 +4,36 @@ import { Component } from '@angular/core';
   selector: 'app-custom-sidenav',
   imports: [],
   template: `
-<div class="sidenav-header">
-  <img width="100" height="100" src="/assets/user.webp">
-</div>
+    <div class="sidenav-header">
+      <img width="100" height="100" src="/assets/user.webp" />
+      <div class="header-text">
+        <h2>Your channel</h2>
+        <p>Christoph</p>
+      </div>
+    </div>
   `,
-  styles: ``
-})
-export class CustomSidenav {
+  styles: `
+  .sidenav-header {
+    padding-top: 24px;
+    text-align: center;
 
-}
+    > img {
+      border-radius: 100%;
+      object-fit: cover;
+      margin-bottom: 10px;
+    }
+
+    .header-text {
+      margin: 0;
+      font-size: 1rem;
+      line-height: 1.5rem;
+    }
+
+    > p {
+      margin: 0;
+      font-size: 0.8rem;
+    }
+  }
+  `,
+})
+export class CustomSidenav {}
