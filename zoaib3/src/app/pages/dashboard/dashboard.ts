df5ef182048a595482cd58d5a37b6a48563cb4a3
Christoph <c.wagner.germany@gmail.com>
2025-07-12 18:47:06 +0200

    elevation

--- zoaib3/src/app/pages/dashboard/dashboard.ts
+++ zoaib3/src/app/pages/dashboard/dashboard.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { WidgetComponent } from "../../components/widget-component/widget-component";
+import { Widget } from '../../models/dashboard';
+import { Subscribers } from './widgets/subscribers';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [WidgetComponent],
   template: `
     <h1>Dashboard</h1>
+
+    <app-widget-component [data]=data></app-widget-component>
   `,
   styles: ``
 })
 export class Dashboard {
+
+  data: Widget = {
+    id: 1,
+    label: 'Subscribers',
+    content: Subscribers
+  }
 
 }
