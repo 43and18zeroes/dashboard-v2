a9e3e46850477c85e1cb33a3e795c92566f4518d
Christoph <c.wagner.germany@gmail.com>
2025-07-13 15:10:33 +0200

    widgetsToAdd

--- zoaib3/src/app/services/dashboard-service.ts
+++ zoaib3/src/app/services/dashboard-service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Subscribers } from '../pages/dashboard/widgets/subscribers';
 import { Widget } from '../models/dashboard';
 import { Views } from '../pages/dashboard/widgets/views';
@@ -17,4 +17,11 @@ export class DashboardService {
       content: Views,
     },
   ]);
+
+  addedWidgets = signal<Widget[]>([]);
+
+  widgetsToAdd = computed(() => {
+    const addedIds = this.addedWidgets().map(w => w.id);
+    return this.widgets().filter(w => !addedIds.includes(w.id));
+  })
 }
