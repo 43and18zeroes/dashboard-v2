6c9eef49b5278f183ca7bc4224a6a9e668098be7
Christoph <c.wagner.germany@gmail.com>
2025-07-13 15:17:04 +0200

    widgetMenu

--- zoaib3/src/app/pages/dashboard/dashboard.ts
+++ zoaib3/src/app/pages/dashboard/dashboard.ts
@@ -3,19 +3,27 @@ import { WidgetComponent } from '../../components/widget-component/widget-compon
 import { DashboardService } from '../../services/dashboard-service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [WidgetComponent, MatButtonModule, MatIconModule],
+  imports: [WidgetComponent, MatButtonModule, MatIconModule, MatMenuModule],
   providers: [DashboardService],
   template: `
-<div class="header">
-    <h2>Channel Dashboard</h2>
-<button mat-raised-button color="primary">
-  <mat-icon>add_circle</mat-icon>
-  Add widget
-</button>
-</div>
+    <div class="header">
+      <h2>Channel Dashboard</h2>
+      <button mat-raised-button color="primary" [mat-menu-trigger-for]="widgetMenu">
+        <mat-icon>add_circle</mat-icon>
+        Add widget
+      </button>
+      <mat-menu #widgetMenu="matMenu">
+        @for (widget of store.widgetsToAdd(); track widget.id) {
+        <button mat-menu-item>
+          {{ widget.label }}
+        </button>
+        }
+      </mat-menu>
+    </div>
     <div class="dashboard-widgets">
       @for (w of store.widgets(); track w.id) {
       <app-widget-component [data]="w"></app-widget-component>
