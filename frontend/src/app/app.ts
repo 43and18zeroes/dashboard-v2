15feeea711c08da1d1323aafc7abd10cb94e4802
Christoph <c.wagner.germany@gmail.com>
2025-07-19 15:09:08 +0200

    new frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,38 +1,12 @@
-import { Component, computed, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CustomSidenav } from './components/custom-sidenav/custom-sidenav';
-import { ThemeService } from './services/theme';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    CustomSidenav,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'dashboard-v2';
-
-  constructor(private themeService: ThemeService) {}
-
-  collapsed = signal(false);
-  sidenavWidth = computed(() => (this.collapsed() ? '75px' : '250px'));
-
-  ngOnInit(): void {
-    this.themeService.loadTheme();
-  }
-
-  toggleTheme(): void {
-    this.themeService.toggleTheme();
-  }
+  protected title = 'frontend';
 }
