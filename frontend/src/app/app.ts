2839c01b4f51589a099c7d56b15d46a4c92a085f
Christoph <c.wagner.germany@gmail.com>
2025-07-19 15:27:49 +0200

    fav

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
