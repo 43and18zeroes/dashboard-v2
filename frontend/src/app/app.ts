33b34ec3437391073d37aea19d2db4b0aa0e0bcb
Christoph <c.wagner.germany@gmail.com>
2025-07-20 14:08:14 +0200

    new ng

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'frontend';
+  protected readonly title = signal('frontend');
 }
