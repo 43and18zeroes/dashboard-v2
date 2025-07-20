bd9fa09ab8751a4f2aee7922195af330c2ad6899
Christoph <c.wagner.germany@gmail.com>
2025-07-20 14:29:34 +0200

    side

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
